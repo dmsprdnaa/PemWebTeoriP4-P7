e3ce0731b84aec0afaf0ca544eb0bb002a3c3cd6
DimasPutra <if23.dimasperdana@mhs.ubpkarawang.ac.id>
2024-11-28 19:25:33 +0700

    Add files via upload

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,64 @@
 <?php
 
-        if(isset($_POST['email']) || isset($_POST['password'])) {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
 
-            if($email == 'admin@admin.com' && $password == 'admin') {
-                header('Location: ./../dashboard.php');
-            } else {
-                echo "email atau password salah";
+// if (isset($_POST["email"]) && isset($_POST["password"])) {
+//     $email = $_POST["email"];
+//     $password = $_POST["password"];
+
+//     if ($email == 'admin123@gmail.com' && $password == 'admin') {
+//         header('Location: ./../beranda.php');
+//     } else {
+//         header('Location: ./../wrong.php');
+//     }
+// }
+
+
+
+session_start();
+require '../config/db.php';
+
+if (isset($_POST['submit'])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Query untuk mencari pengguna berdasarkan email
+    $user = mysqli_query($db_connect, "SELECT * FROM users WHERE email = '$email'");
+
+    if (mysqli_num_rows($user) > 0) {
+        $data = mysqli_fetch_assoc($user);
+
+        // Verifikasi password
+        if (password_verify($password, $data['password'])) {
+
+            // Otorisasi
+            $_SESSION['name'] = $data['name'];
+            $_SESSION['role'] = $data['role'];
+
+            // Redirect berdasarkan role
+            if ($_SESSION['role'] == 'admin') {
+                if (file_exists('../profile.php')) {
+                    echo "File admin.php ditemukan.";
+                    header('Location: ../profile.php');
+                } else {
+                    echo "File admin.php tidak ditemukan di path ../profile.php";
+                }
+                exit;
             }
+             else {
+                echo "Redirecting to user.php...";
+                header('Location: ../user.php');
+                exit;
+            }
+            
+        } else {
+            // Password salah
+            echo "Password Salah";
+            exit;
         }
-    ?>
+    } else {
+        // Email tidak ditemukan
+        echo "Email Salah";
+        exit;
+    }
+}
